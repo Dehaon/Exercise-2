2c988fca41f381c359cd08cc514fffe2073a6216
Dehaon <115060223+Dehaon@users.noreply.github.com>
2022-10-11 19:56:25 +0600

    2

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log('Запускаем таймер');
+
+alert(5);
+alert(4);
+alert(3);
+alert(2);
+alert(1);
+alert(0);
+
+console.log('Обратный отсчет закончен');
+
+
+
+// console.log('Hello World 3');
+
+/* alert("Hellow World");
+alert('12313'); */
+
+// prompt('What is your sign?', '1234');
